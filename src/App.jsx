42f14aa32416f2580247761f0e3373be662ef855
Bineth <binethellepola@gmail.com>
2025-12-17 18:54:12 +0530

    My

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -7,17 +8,25 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+// Import ALL CSS files
+import "./styles/global.css";
+
+
 function App() {
   return (
-    <div className="app-root">
-      <Header />
     
+    
+    <div className="app-wrapper">
+      <Header />
+
+      <main>
         <Hero />
         <About />
         <Skills />
         <Projects />
         <Contact />
-     
+      </main>
+
       <Footer />
     </div>
   );
